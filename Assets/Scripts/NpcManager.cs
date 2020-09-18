1f11ce8bc5cc4cf9ea7f343033b4d7c727d63fda
ds0605 <ds0605@edu.turku.fi>
2020-09-18 14:44:41 +0300

    ss
    ss

---
+++ Assets/Scripts/NpcManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float animalSpeed = 2;
+        Vector3 position = this.transform.position;
+        position.x = position.x + animalSpeed;
+        this.transform.position = position;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        verticalInput = Input.GetAxis("Horizontal");
+        Vector3 position = this.transform.position;
+        position.x = position.x + (this.MovementSpeed * verticalInput * Time.deltaTime);
+        this.transform.position = position;
+    }
+}
